c1292bd64ecd25545db001fdfa1f655be7db259b
po-lar <po-lar@mbp-polar.local>
2018-03-22 00:47:43 +0900

    modified

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,6 +1,7 @@
 
 # mac_setup.sh
 
+# mac setup
 defaults write -g InitalKeyRepeat -int 0
 defaults write -g KeyRepeat -int 0
 defaults write com.apple.finder AppleShowAllFiles -bool true
@@ -9,9 +10,27 @@ defaults write com.apple.Dock autohide-delay -float 0
 killall Finder
 killall Dock
 
+# xcode command line tools install 
+xcode-select --install
+
+# alcatraz install
+curl -fsSL https://raw.githubusercontent.com/supermarin/Alcatraz/deploy/Scripts/install.sh | sh
+gem install update_xcode_plugins
+update_xcode_plugins
+update_xcode_plugins --unsign
+
+# xvim2 install
+cd ~/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins/
+git clone git://github.com/XVimProject/XVim2 XVim2
+cd XVim2
+make
+cd ~
+
+# homebrew install 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
 
 brew update
 brew upgrade
 
+# Brewfile install
 brew bundle
--- shell_setup.sh
+++ shell_setup.sh
@@ -1,8 +1,17 @@
 
 # shell_setup.sh
 
+# set zsh default shell
 echo "/usr/local/bin/zsh" | sudo tee /etc/shells
 chsh -s /usr/local/bin/zsh
-exec $SHELL
 
+# install prezto
+cd ~
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# set prompt
+zsh -c "$(curl -fsSL https://raw.githubusercontent.com/el1t/statusline/master/prezto/install)"
