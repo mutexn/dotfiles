5b96a6b01068cbb04ee44a4e3619f08ecc755bf8
mutexn <k.shoma.0810.mf@gmail.com>
2019-04-01 18:42:31 +0900

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 
 # mac_setup.sh
+echo"------super user-----------------------------------------------------"
 
 su
 
+echo"---------------------------------------------------------------------"
+
+
 # mac setup
+
+echo"------mac setup------------------------------------------------------"
+
 defaults write -g InitalKeyRepeat -int 0
 defaults write -g KeyRepeat -int 0
 defaults write com.apple.finder AppleShowAllFiles -bool true
@@ -13,34 +20,75 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores True
 killall Finder
 killall Dock
 
+echo"---------------------------------------------------------------------"
+
+
 # xcode command line tools install 
+
+echo"------xcode command line tools install-------------------------------"
+
 xcode-select --install
 
+echo"---------------------------------------------------------------------"
+
+
 # alcatraz install
+
+echo"------alcatraz install-----------------------------------------------"
+
 curl -fsSL https://raw.githubusercontent.com/supermarin/Alcatraz/deploy/Scripts/install.sh | sh
 gem install update_xcode_plugins
 update_xcode_plugins
 update_xcode_plugins --unsign
 
+echo"---------------------------------------------------------------------"
+
+
 # xvim2 install
+
+echo"------alcatraz install-----------------------------------------------"
 
 make
 cd ~
 
+echo"---------------------------------------------------------------------"
+
+
 # homebrew install 
+
+echo"------homebrew install-----------------------------------------------"
+
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
 
 brew update
 brew upgrade
 
+echo"---------------------------------------------------------------------"
+
+
 # Brewfile install
+
+echo"------Brewfile install-----------------------------------------------"
+
 brew bundle
 
+echo"---------------------------------------------------------------------"
+
+
 # set zsh default shell
+
+echo"------zsh defaults setup---------------------------------------------"
+
 echo "/usr/local/bin/zsh" | sudo tee /etc/shells
 chsh -s /usr/local//bin/zsh
 
+echo"---------------------------------------------------------------------"
+
+
 # install prezto
+
+echo"------prezto install-------------------------------------------------"
+
 cd ~
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.prezto"
 setpot EXTENDED_GLOB
@@ -48,5 +96,13 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
+echo"---------------------------------------------------------------------"
+
+
 #set prompt
+
+echo"------prompt install-------------------------------------------------"
+
 zsh -c "$(curl -fsSL https://raw.githubusercontent.com/el1t/statusline/master/prezto/install)"
+
+echo"---------------------------------------------------------------------"
