93c99e8220ecfec6739be3cf7b4d5155cd65ea7d
mutexn <k.shoma.0810.mf@gmail.com>
2020-11-24 06:31:59 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,28 +32,6 @@ xcode-select --install
 echo"---------------------------------------------------------------------"
 
 
-# alcatraz install
-
-echo"------alcatraz install-----------------------------------------------"
-
-curl -fsSL https://raw.githubusercontent.com/supermarin/Alcatraz/deploy/Scripts/install.sh | sh
-gem install update_xcode_plugins
-update_xcode_plugins
-update_xcode_plugins --unsign
-
-echo"---------------------------------------------------------------------"
-
-
-# xvim2 install
-
-echo"------alcatraz install-----------------------------------------------"
-
-make
-cd ~
-
-echo"---------------------------------------------------------------------"
-
-
 # homebrew install 
 
 echo"------homebrew install-----------------------------------------------"
